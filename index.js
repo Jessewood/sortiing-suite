6126f9f5f2e7adb387fc63d7b1daec0aadf7a7ab
Jesse Packwood <packwoodjustin@gmail.com>
2017-10-23 13:52:41 -0600

    moved index.js to root folder

---
+++ index.js
@@ -0,0 +1,11 @@
+const bubbleSort = require('./scripts/bubble.js');
+const insertionSort = require('./scripts/insertion.js');
+const mergeSort = require('./scripts/merge.js');
+const quickSort = require('./scripts/quick.js');
+
+module.exports = {
+  bubbleSort,
+  insertionSort,
+  mergeSort,
+  quickSort
+}
